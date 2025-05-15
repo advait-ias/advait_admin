8d6eeca63b2158962265480671de187ba835ca22
vkkd-dev <vargabdas@gmail.com>
2025-05-15 08:14:04 +0530

    added courses, tests and live classes

--- src/pages/liveclasses/LiveClasses.tsx
+++ src/pages/liveclasses/LiveClasses.tsx
@@ -1,43 +1,5 @@
 import { useState } from "react";
-import { GridColDef } from "@mui/x-data-grid";
-import Add from "../../components/add/Add";
 import "./liveclasses.scss";
-
-const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 250 },
-  {
-    field: "img",
-    headerName: "Icon",
-    width: 75,
-    renderCell: (params) => {
-      return <img src={params.row.img || "/noavatar.png"} alt="" />;
-    },
-  },
-  {
-    field: "category",
-    headerName: "Course Category",
-    width: 200,
-    type: "string",
-  },
-  {
-    field: "name",
-    type: "string",
-    headerName: "Course Name",
-    width: 200,
-  },
-  {
-    field: "email",
-    type: "string",
-    headerName: "Duration",
-    width: 150,
-  },
-  {
-    field: "phone",
-    type: "number",
-    headerName: "Price",
-    width: 150,
-  },
-];
 
 const LiveClasses = () => {
   const [open, setOpen] = useState(false);
@@ -48,7 +10,6 @@ const LiveClasses = () => {
         <h1>Live Classes</h1>
       </div>
       <p className="noData">Coming Soon</p>
-      {open && <Add slug="student" columns={columns} setOpen={setOpen} />}
     </div>
   );
 };
